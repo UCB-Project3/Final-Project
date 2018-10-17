87cea840ef332acc1cef7fe3b3b21bc21627508f
Alex Camacho <alex.c@WalkMe.local>
2018-10-17 13:56:30 -0700

    added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
 import Wrapper from "./components/Wrapper";
 import Nav from "./components/Nav";
 //import Title from "./components/Title";
@@ -31,10 +36,20 @@ import './App.css';
 
 
 const App = () => (
-  <Wrapper>
-    <Nav />
-
-  </Wrapper>
+  <Router>
+    <Wrapper>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/profile" component={Profile} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Wrapper>
+  </Router>
 );
 
 export default App;
