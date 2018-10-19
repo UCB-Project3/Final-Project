d7e93c9b0c1bd624ba404692be907d72db6002af
unknown <maryamshokoohhi@gmail.com>
2018-10-18 18:45:38 -0700

    banner fixed

--- client/src/components/banner/banner.js
+++ client/src/components/banner/banner.js
@@ -1,17 +1,15 @@
 import React from "react";
-import pic from "../../img/Welcome.jpg"
-import './banner.css'
+import './Banner.css'
 
-const Banner = () => (
+
+const Banner = props => (
 
 
     <section className="front-page-section">
         <div className="front-page">
             <div id="bannerContainer">
                 <div class="welcome-premium-banner welcome-premium-banner--get-premium">
-                    <div class="welcome-premium-banner__header welcome-premium-banner__header--inverted">Search any wine and buy in seconds</div>
-                    <img src={pic} alt="picture" />;
-                    <Image source={require('../../img/Welcome.jpg')} />
+                    <div class="welcome-premium-banner__header welcome-premium-banner__header--inverted">Search any Cosmetics and buy in seconds</div>
                 </div>
             </div>
         </div>
