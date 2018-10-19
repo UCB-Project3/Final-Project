8a13e8827e9be42407135e5de768282b00196b2e
Alex Camacho <alex.c@WalkMe.local>
2018-10-18 20:04:21 -0700

    added templates for all of the pages

---
+++ client/src/pages/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>Test</div>
+        );
+    }
+}
+
+export default Profile;
---
+++ client/src/pages/Search/Search.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Search extends Component {
+    render() {
+        return (
+            <div>Search page</div>
+        );
+    }
+}
+
+export default Search;
