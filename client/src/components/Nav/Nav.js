5441fcbd97772843adbd547d2632e8db8282f9e4
Alex Camacho <alex.c@WalkMe.local>
2018-10-18 21:30:20 -0700

    set up beginning of search functionality

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import Dropdown from "../dropdownmenu/Dropdown";
 import SearchBtn from "../SearchBtn/SearchBtn";
 import './Nav.css'
+import products from '../../data/products.json';
 
-const Nav = () => (
-  <div className="navigation-container">
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <div className="srchdiv">
-          <SearchBtn />
-        </div>
-        <div className="dropdowndiv">
-          <Dropdown />
-        </div>
-      </div>
-    </nav>
-  </div >
-);
+class Nav extends Component {
+  state = {
+    searchTerm: "",
+    results: []
+  }
+
+  searchJSON = () => {
+    const term = this.state.searchTerm;
+
+    products.forEach(product => {
+      if (product.name.indexOf(term) > -1 || product.category.indexOf(term) > -1 || product.product_type.indexOf(term) > -1) {
+        results.push(product);
+      }
+    })
+
+
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    this.searchJSON();
+
+  }
+
+  render() {
+    return (
+      <div className="navigation-container">
+        <nav className="navbar">
+          <div className="navbar-brand">
+            <div className="srchdiv">
+              <SearchBtn />
+            </div>
+            <div className="dropdowndiv">
+              <Dropdown />
+            </div>
+          </div>
+        </nav>
+      </div >
+    )
+  }
+}
 
 export default Nav;
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import SearchResult from "../../components/SearchResult";
 
 class Search extends Component {
+    state = {
+        results
+    };
+
     render() {
         return (
-            <div>Search page</div>
+            <Wrapper>
+                <div className="row no-gutters">
+                    <div className="col-6 col-md-4">
+                        filters placeholder
+                </div>
+                    <div className="col-12 col-sm-6 col-md-8">
+                    </div>
+                </div>
+
+                {this.state.results.map(result => (
+                    <SearchResult
+                        image={result.image}
+                        brandname={result.brandname}
+                        productname={result.productname}
+                        rating={result.rating}
+                        price={result.price}
+                    />
+                ))
+                }
+            </Wrapper>
         );
     }
 }
