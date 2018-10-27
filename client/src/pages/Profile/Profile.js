1a809be0d0931440e7b5f9d3e0b4856805ed0fd1
tra <nellyng2013@gmail.com>
2018-10-27 13:17:49 -0700

    bnm

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import "./Profile.css";
+
 
 class Profile extends Component {
     render() {
         return (
-            <div>Profile page</div>
-        );
-    }
-}
+            
+
+    
 
 export default Profile;
