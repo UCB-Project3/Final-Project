1714cda0941b09e2408aeed471c0bb5bc58cebda
tra <nellyng2013@gmail.com>
2018-10-29 18:20:08 -0700

    updateP

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -13,10 +13,34 @@ import {
 import ReactDOM from "react-dom";
 import "./Profile.css";
 
+// import Avatar from "../../Avatar"
+
+
 
 class Profile extends Component {
     render() {
         return (
+            <div>
+            <Row>
+            <div className="col-md-12 column">
+              <Banner />
+            </div>
+            </Row>
+            <Row>
+            <div className="col-md-4 column">
+                <span>here is the pix</span>
+                </div>
+            <div className="col-md-8 column">
+                <Card>
+                    <span>**We suggest you These top 10 products</span>
+                </Card>
+                </div>
+                
+            </Row>
+            </div>
+        );
+      }
+    }  
             
     
 
